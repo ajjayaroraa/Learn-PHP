86430ac5e1ec95eca0525e8e0fa130a0348752dc
ajjayaroraa <ajjayaroraa@gmail.com>
2020-05-17 18:16:46 +0530

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,20 @@
+<?php
+
+// Single Line Comment
+
+/*
+Multi Line Comment
+*/
+
+
+/*
+Demo of echo tag
+*/
+echo "This is <b>echo</b> tag<br />";
+
+/*
+Demo of print tag
+*/
+print "This is <b>print</b> tag";
+
+?>
